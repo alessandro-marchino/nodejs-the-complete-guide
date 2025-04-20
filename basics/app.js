7d4fe20c0c30291c8897c8098f3af375af89479e
amarchino <marchino.alessandro@gmail.com>
2025-04-20 12:06:40 +0200

    Lesson 31

--- basics/app.js
+++ basics/app.js
@@ -2,5 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</body>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(3000);
