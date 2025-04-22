6b73eb54f4a69b7ae18b3bc377b80fa92cca8737
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-22 13:57:57 +0200

    Lesson 34

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html');
@@ -9,6 +11,12 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>Enter message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('output/message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
