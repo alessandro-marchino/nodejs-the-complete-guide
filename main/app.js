16104919aa4bd6c6fe772bcb38d439c75d01bf09
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-24 14:19:46 +0200

    Lesson 66

--- main/app.js
+++ main/app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (_, res) => {
+app.get('/add-product', (_, res) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
-app.use('/product', (req, res) => {
+app.post('/product', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
