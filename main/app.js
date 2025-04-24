a892b16b0d2ef27519fac28a1e04517fe86bc208
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-24 14:03:55 +0200

    Lesson 64

--- main/app.js
+++ main/app.js
@@ -1,15 +1,18 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 app.use((req, res, next) => {
-    console.log('In the middleware');
+    console.log('This always runs');
     next();
 });
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>The "Add Product" page</h1>')
+});
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express!</h1>')
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
