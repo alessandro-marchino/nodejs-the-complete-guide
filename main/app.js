3fac98954d895e6bf41037bf8b5558d5461e5c06
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-24 14:18:37 +0200

    Lesson 65

--- main/app.js
+++ main/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('This always runs');
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>The "Add Product" page</h1>')
+app.use('/add-product', (_, res) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
-app.use('/', (req, res, next) => {
-    console.log('In another middleware');
+app.use('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/', (_, res) => {
     res.send('<h1>Hello from Express!</h1>')
 });
 
