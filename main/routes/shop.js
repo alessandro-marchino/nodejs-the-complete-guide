28cb4a8e99dac29d88b7acfeda902dac2c36f8a0
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-28 09:01:55 +0200

    Lesson 74

--- main/routes/shop.js
+++ main/routes/shop.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const rootDir = require('../util/path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (_, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
