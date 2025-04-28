09844e8335319daafa815fd4fea03d179d53587f
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-28 08:30:36 +0200

    Lesson 67

--- main/app.js
+++ main/app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/add-product', (_, res) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (_, res) => {
-    res.send('<h1>Hello from Express!</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
