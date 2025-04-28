abb013097d631c605dc14e0b6c25eb648a508518
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-28 08:34:01 +0200

    Lesson 68

--- main/app.js
+++ main/app.js
@@ -9,4 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((_, res) => {
+    res
+        .status(404)
+        .send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);
--- main/routes/shop.js
+++ main/routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-app.use('/', (_, res) => {
+router.get('/', (_, res) => {
     res.send('<h1>Hello from Express!</h1>')
 });
 
