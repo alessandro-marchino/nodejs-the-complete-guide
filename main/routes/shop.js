e4973f100f8b0ba4b533e57a3aca90bcf89d278e
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-04-28 08:57:09 +0200

    Lesson 71

--- main/routes/shop.js
+++ main/routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (_, res) => {
-    res.send('<h1>Hello from Express!</h1>')
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
