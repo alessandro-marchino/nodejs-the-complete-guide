68ffb7f59b98d45e1844842ca362bb72b80e2bad
amarchino <marchino.alessandro@gmail.com>
2025-05-02 21:01:40 +0200

    Lesson 104

--- main/model/product.js
+++ main/model/product.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 const basePath = require('../util/path');
-const products = [];
 const p = path.join(basePath, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            return cb([]);
+        }
+        return cb(JSON.parse(fileContent));
+    });
+}
 
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
     save() {
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if(!err) {
-                console.log(fileContent)
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 if(err) {
@@ -24,11 +27,6 @@ module.exports = class Product {
         });
     }
     static fetchAll(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {
-                return cb([]);
-            }
-            return cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
