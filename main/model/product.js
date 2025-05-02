3987721f596315c5f20372e9c590ca7f9b13cd10
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-05-02 15:56:06 +0200

    Lesson 102

--- main/model/product.js
+++ main/model/product.js
@@ -1,13 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const basePath = require('../util/path');
 const products = [];
+const p = path.join(basePath, 'data', 'products.json');
 
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
     save() {
-        products.push(this);
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if(!err) {
+                console.log(fileContent)
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                if(err) {
+                    console.log(err);
+                }
+            });
+        });
     }
     static fetchAll() {
-        return [...products];
+        fs.readFile(p, (err, fileContent) => {
+            if(!err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
     }
 }
