8aeeb624dbae3a2a034e72acf646eb71fa6e41c8
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-05-02 08:49:10 +0200

    Lesson 83

--- main/routes/shop.js
+++ main/routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { products } = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (_, res) => {
-    res.render('shop');
+    res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
