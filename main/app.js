365ebe897bb76cc682b22f4e870ab84317ebb0a0
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-06-09 15:22:58 +0200

    Lesson 212

--- main/app.js
+++ main/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const path = require('path');
 
-const { mongoConnect } = require('./util/database');
-const User = require('./model/user');
+// const { mongoConnect } = require('./util/database');
+// const User = require('./model/user');
 
 const app = express();
 
@@ -17,12 +18,13 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-    User.findById('684682f4358f90c920641fd5')
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(e => console.error(e));
+    // User.findById('684682f4358f90c920641fd5')
+    //     .then(user => {
+    //         req.user = new User(user.name, user.email, user.cart, user._id);
+    //         next();
+    //     })
+    //     .catch(e => console.error(e));
+    next();
 })
 
 app.use('/admin', adminRoutes);
@@ -30,4 +32,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => app.listen(3000))
+mongoose.connect('mongodb://nodejscomplete:mypass@localhost:27017/?authSource=nodejscomplete')
+    .then(() => app.listen(3000))
+    .catch(err => console.error(err));
--- main/model/product.js
+++ main/model/product.js
@@ -1,42 +1,42 @@
-const mongodb = require('mongodb');
-const { getDb } = require('../util/database');
+// const mongodb = require('mongodb');
+// const { getDb } = require('../util/database');
 
-class Product {
-    constructor(title, price, description, imageUrl, userId, id) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.userId = userId;
-        this._id = id ? mongodb.ObjectId.createFromHexString(id) : null;
-    }
+// class Product {
+//     constructor(title, price, description, imageUrl, userId, id) {
+//         this.title = title;
+//         this.price = price;
+//         this.description = description;
+//         this.imageUrl = imageUrl;
+//         this.userId = userId;
+//         this._id = id ? mongodb.ObjectId.createFromHexString(id) : null;
+//     }
 
-    save() {
-        const db = getDb();
-        console.log(this)
-        if(this._id) {
-            return db.collection('products').updateOne({ _id: this._id }, { $set: this });
-        }
-        return db.collection('products').insertOne(this);
-    }
+//     save() {
+//         const db = getDb();
+//         console.log(this)
+//         if(this._id) {
+//             return db.collection('products').updateOne({ _id: this._id }, { $set: this });
+//         }
+//         return db.collection('products').insertOne(this);
+//     }
 
-    static fetchAll() {
-        return getDb()
-            .collection('products')
-            .find()
-            .toArray();
-    }
-    static findById(productId) {
-        return getDb()
-            .collection('products')
-            .find({ _id: mongodb.ObjectId.createFromHexString(productId) })
-            .next();
-    }
-    static deleteById(productId) {
-        return getDb()
-            .collection('products')
-            .deleteOne({ _id: mongodb.ObjectId.createFromHexString(productId) })
-    }
-}
+//     static fetchAll() {
+//         return getDb()
+//             .collection('products')
+//             .find()
+//             .toArray();
+//     }
+//     static findById(productId) {
+//         return getDb()
+//             .collection('products')
+//             .find({ _id: mongodb.ObjectId.createFromHexString(productId) })
+//             .next();
+//     }
+//     static deleteById(productId) {
+//         return getDb()
+//             .collection('products')
+//             .deleteOne({ _id: mongodb.ObjectId.createFromHexString(productId) })
+//     }
+// }
 
-module.exports = Product;
+// module.exports = Product;
