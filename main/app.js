1af1c6a19dc4da00c8c0d63be6a5fa8dcf77eb26
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-06-16 08:47:54 +0200

    Lesson 242

--- main/app.js
+++ main/app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import { connect } from 'mongoose';
 import { join } from 'path';
 import Session from 'express-session';
+import MongoDbStore from 'connect-mongodb-session';
 
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
@@ -11,14 +12,20 @@ import * as errorController from './controllers/error.js';
 
 import User from './model/user.js';
 
+const MONGODB_URI = 'mongodb://nodejscomplete:mypass@localhost:27017/nodejscomplete?authSource=nodejscomplete';
+
 const app = express();
+const store = new (MongoDbStore(Session))({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(join(import.meta.dirname, 'public')));
-app.use(Session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(Session({ secret: 'my secret', resave: false, saveUninitialized: false, store }));
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.session?.isLoggedIn;
     next();
@@ -38,7 +45,7 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-connect('mongodb://nodejscomplete:mypass@localhost:27017/nodejscomplete?authSource=nodejscomplete')
+connect(MONGODB_URI)
     .then(() => User.findOne({ name: 'Max' }))
     .then(user => {
         if(user) {
