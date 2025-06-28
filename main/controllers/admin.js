27cf4759e6336c5f073c56f285dcd3be4a30f45a
amarchino <marchino.alessandro@gmail.com>
2025-06-28 17:18:42 +0200

    Lesson 303

--- main/controllers/admin.js
+++ main/controllers/admin.js
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator';
 import Product from "../model/product.js";
 
 export function getAddProduct(req, res) {
@@ -5,6 +6,10 @@ export function getAddProduct(req, res) {
 }
 
 export function postAddProduct(req, res) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', { pageTitle: 'Add Product', path: '/admin/add-product', editing: false, errorMessage: errors.array()[0].msg, validationErrors: errors.mapped() });
+    }
     const product = new Product({
         title: req.body.title,
         price: req.body.price,
@@ -33,6 +38,11 @@ export function getEditProduct(req, res) {
 }
 
 export function postEditProduct(req, res) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', { pageTitle: 'Edit Product', path: '/admin/edit-product', editing: true, errorMessage: errors.array()[0].msg, validationErrors: errors.mapped() });
+    }
+
     Product.findById(req.body.productId)
         .then(product => {
             if(!product.userId._id.equals(req.user._id)) {
--- main/routes/admin.js
+++ main/routes/admin.js
@@ -1,15 +1,40 @@
 import { Router } from 'express';
 import * as adminController from '../controllers/admin.js';
 import isAuth from '../middleware/is-auth.js';
+import { body } from 'express-validator';
 
 const router = Router();
 
 router.use(isAuth);
 
 router.get('/add-product', adminController.getAddProduct);
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', [
+    body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isLength({ min: 5, max: 400 })
+        .trim()
+], adminController.postAddProduct);
 router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', [
+    body('title')
+        .isAlphanumeric()
+        .isLength({ min: 3 })
+        .trim(),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isLength({ min: 5, max: 400 })
+        .trim()
+], adminController.postEditProduct);
 router.post('/delete-product', adminController.postDeleteProduct);
 router.get('/products', adminController.getProducts);
 
