f7907cd0f2e25e1bba6028b7ace5963706dfc684
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-07-14 09:46:15 +0200

    Lesson 368

--- rest/public/index.js
+++ rest/public/index.js
@@ -7,4 +7,15 @@ getButton.addEventListener('click', () => {
     .then(resData => console.log(resData))
     .catch(err => console.log(err));
 });
-postButton.addEventListener('click', () => {});
+postButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({title: 'My Second Post!', content: 'This is the content of my second post.'})
+  })
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(err => console.log(err));
+});
