11e763e0b4c4cfb6b2097b5dcd2526b29a0e0246
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-07-14 09:41:19 +0200

    Lesson 367

--- rest/app.js
+++ rest/app.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import feedRouter from './routes/feed.js';
+import { join } from 'path';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
 
 app.use('/feed', feedRouter);
 app.listen(8080);
+
+const fe = express();
+fe.use(express.static(join(import.meta.dirname, 'public')));
+fe.get('', (req, res, next) => res.send('<script src="index.js" defer></script><button id="get">Get Posts</button><button id="post">Create a Post</button>'));
+fe.listen(3000);
---
+++ rest/public/index.js
@@ -0,0 +1,10 @@
+const getButton = document.getElementById('get');
+const postButton = document.getElementById('post');
+
+getButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/posts')
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(err => console.log(err));
+});
+postButton.addEventListener('click', () => {});
