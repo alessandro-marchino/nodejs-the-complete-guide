aaee1da774c8fcf6d356f0ca525a08793ebc2482
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-07-21 14:36:44 +0200

    Assignment 8

--- rest/routes/auth.ts
+++ rest/routes/auth.ts
@@ -9,13 +9,11 @@ const router = Router();
 router.put('/signup', [
   body('email')
     .isEmail().withMessage('Please enter a valid email')
-    .custom((value, { req }) => {
-      return User.findOne({ email: value })
-        .then(userDoc => {
-          if(userDoc) {
-            return Promise.reject('E-Mail address already exists');
-          }
-        })
+    .custom(async (value) => {
+      const userDoc = await User.findOne({ email: value });
+      if(userDoc) {
+        throw new Error('E-Mail address already exists');
+      }
     })
     .normalizeEmail(),
     body('password').trim().isLength({ min: 5 }),
