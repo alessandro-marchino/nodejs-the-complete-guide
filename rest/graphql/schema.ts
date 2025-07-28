ba63579d0e047961fc0dabf84394359a0bb1928e
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-07-28 09:12:48 +0200

    Lesson 424

--- rest/graphql/schema.ts
+++ rest/graphql/schema.ts
@@ -1,17 +1,37 @@
 import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
-  type TestData {
-    text: String!
-    views: Int!
+  input UserInputData {
+    email: String!
+    name: String!
+    password: String!
   }
 
-  type RootQuery {
-    hello: TestData!
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+    posts: [Post!]!
+  }
+
+  type RootMutation {
+    createUser(userInput: UserInputData): User!
   }
 
   schema {
-    query: RootQuery
+    mutation: RootMutation
   }
 `);
 
