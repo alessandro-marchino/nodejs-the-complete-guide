f317f6b5dcb9db1b3c451988455ae78ac04ac4c3
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-08-04 14:15:25 +0200

    Lesson 509

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import todosRoutes from './routes/todos';
+import { json } from 'body-parser';
 
 const app = express();
+app.use(json())
 app.use('/todos', todosRoutes);
 app.listen(3000);
--- typescript/src/routes/todos.ts
+++ typescript/src/routes/todos.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
+import { Todo } from "../models/todo";
+import { randomUUID } from "node:crypto";
 
-const TODOS: any[] = [];
+const MY_TODOS: Todo[] = [];
 
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({ todos: TODOS });
+  res.status(200).json({ todos: MY_TODOS });
 });
+
+router.post('/', (req, res, next) => {
+  const newTodo: Todo = { id: randomUUID(), text: req.body.text };
+  MY_TODOS.push(newTodo);
+  res.status(201);
+})
 
 export default router;
