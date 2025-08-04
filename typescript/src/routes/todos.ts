cf2d0c4caecce29cae0b0c779f3dfbd383ac614e
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-08-04 14:26:54 +0200

    Lesson 512

--- typescript/src/routes/todos.ts
+++ typescript/src/routes/todos.ts
@@ -3,6 +3,8 @@ import { Todo } from "../models/todo";
 import { randomUUID } from "node:crypto";
 
 let MY_TODOS: Todo[] = [];
+type RequestBody = { text: string };
+type RequestParams = { id: string };
 
 const router = Router();
 
@@ -11,27 +13,32 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const newTodo: Todo = { id: randomUUID(), text: req.body.text };
+  const body = req.body as RequestBody;
+  const newTodo: Todo = { id: randomUUID(), text: body.text };
   MY_TODOS.push(newTodo);
   res.status(201).json({ message: 'Added todo', todo: newTodo, todos: MY_TODOS });
 });
 
 router.put('/:id', (req, res, next) => {
-  const originalTodoIndex = MY_TODOS.findIndex(todo => todo.id === req.params.id);
+  const body = req.body as RequestBody;
+  const params = req.params as RequestParams;
+  const originalTodoIndex = MY_TODOS.findIndex(todo => todo.id === params.id);
   if(originalTodoIndex === -1) {
     return res.status(404).json({ message: 'Could not find todo for this id '});
   }
-  MY_TODOS[originalTodoIndex] = { ...MY_TODOS[originalTodoIndex]!, text: req.body.text };
+  MY_TODOS[originalTodoIndex] = { ...MY_TODOS[originalTodoIndex]!, text: body.text };
   res.status(200).json({ message: 'Updated todo', todos: MY_TODOS });
 });
 
 router.delete('/:id', (req, res, next) => {
-  MY_TODOS = MY_TODOS.filter(todo => todo.id !== req.params.id);
+  const params = req.params as RequestParams;
+  MY_TODOS = MY_TODOS.filter(todo => todo.id !== params.id);
   res.status(200).json({ message: 'Deleted todo', todos: MY_TODOS });
 });
 
 router.get('/:id', (req, res, next) => {
-  const originalTodoIndex = MY_TODOS.findIndex(todo => todo.id === req.params.id);
+  const params = req.params as RequestParams;
+  const originalTodoIndex = MY_TODOS.findIndex(todo => todo.id === params.id);
   if(originalTodoIndex === -1) {
     return res.status(404).json({ message: 'Could not find todo for this id '});
   }
