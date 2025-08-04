177bee3aa67ffa6bdfc468645bd55b92b555c3ac
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-08-04 13:56:13 +0200

    Lesson 493

--- modern-js/response-handler.js
+++ modern-js/response-handler.js
@@ -1,5 +1,7 @@
+import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
-export const resHandler = (req, res) => {
-  res.sendFile(join(import.meta.dirname, 'my-page.html'));
+export const resHandler = async (req, res) => {
+  const content = await readFile(join(import.meta.dirname, 'my-page.html'), 'utf8');
+  res.send(content);
 };
