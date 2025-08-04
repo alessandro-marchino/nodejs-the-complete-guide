76a78608d59d8147a0a08c6d4763b260b87a483f
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-08-04 14:51:03 +0200

    Lesson 521

--- deno/app.ts
+++ deno/app.ts
@@ -1,3 +1,7 @@
-let message: string;
-message = 'Hi there'!
-console.log(message);
+const text = 'This is a test - and it should be stored in a file!';
+
+const textEncoder = new TextEncoder();
+const data = textEncoder.encode(text);
+
+await Deno.writeFile('message.txt', data);
+console.log('Wrote to file');
